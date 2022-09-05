f8f88a02583b13f2bd181592d05c42ad060881e5
eslammk <112516483+eslammk@users.noreply.github.com>
2022-09-05 19:42:39 +0300

    done

---
+++ day1.java
@@ -0,0 +1,16 @@
+public class Eslam5 {
+    public static void main(String[] args) {
+        String myName = "Eslam" ;
+        int age = 23;
+        System.out.println("Hi , My name is " + myName + " and I am " + age + " years old ");  
+        
+        //
+        
+         double height = 160; 
+     if (height >= 160) { 
+         System.out.println("you can enter the play");
+     } else { 
+           System.out.println("you can not enter the play");   
+     }
+    }
+}
